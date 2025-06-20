1dc8f207a3c8cc00b1bd5d601da7f856899b522a
Palika Khurana <73402498+pkhurana-whjr@users.noreply.github.com>
2025-06-20 20:52:12 +0530

    Add files via upload

---
+++ studentcnl/face_detect.py
@@ -0,0 +1,20 @@
+import cv2
+
+img = cv2.imread("boy.jpg")
+
+gray= cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# load file
+
+# change to grayscale
+
+# print
+
+# display the rectangle on the face
+             
+# display the picture
+
+cv2.waitKey(0)
+
+
+
